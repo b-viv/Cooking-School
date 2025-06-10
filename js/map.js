44b358dfa5d2d0e1b9c30450da654ffb2a8fd276
b-vivien <134312026+b-viv@users.noreply.github.com>
2025-06-10 20:31:58 +0200

    Maps API Update

--- js/map.js
+++ js/map.js
@@ -1,20 +1,26 @@
-function initMap() {
-  let map;
+async function initMap() {
 
-  map = new google.maps.Map(document.getElementById("map"), {
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  const map = new Map(document.getElementById("map"), {
     center: { lat: 47.552689, lng: 19.072796 },
     zoom: 15,
     mapId: '7238c4fbd6b40fe7',
   });
 
-  new google.maps.Marker({
-    position: { lat: 47.552689, lng: 19.072796 }, map,
-    title: "Cooking Scool",
-    icon: {
-      url: "assets/images/map_marker.png",
-      scaledSize: new google.maps.Size(100, 100)
-    }
+  const iconImage = document.createElement("img");
+  iconImage.src = "assets/images/map_marker.png";
+  iconImage.style.width = "100px";
+  iconImage.style.height = "100px";
+
+  const marker = new AdvancedMarkerElement({
+    map,
+    position: { lat: 47.552689, lng: 19.072796 },
+    title: "Cooking School",
+    content: iconImage, 
   });
+
 }
 
 window.initMap = initMap;
